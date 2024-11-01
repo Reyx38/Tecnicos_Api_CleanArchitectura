ec237e7075905938f712a03e435e33f00074a57b
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-01 00:31:43 -0400

    Se agrego el contexto

--- Tecnico.Data/Context/ClientesContext.cs
+++ Tecnico.Data/Context/ClientesContext.cs
@@ -3,7 +3,7 @@ using Tecnico.Data.Models;
 
 namespace Tecnico.Data.Context
 {
-    internal class ClientesContext(DbContextOptions<ClientesContext> options) : DbContext(options)
+    public class ClientesContext(DbContextOptions<ClientesContext> options) : DbContext(options)
     {
         public DbSet<Clientes> Clientes { get; set; } 
     }
