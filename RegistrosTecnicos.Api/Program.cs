85279c17d9a4adae89df91a234e0cff3b3688a93
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-11-02 22:22:57 -0400

    cambios para publicar

--- RegistrosTecnicos.Api/Program.cs
+++ RegistrosTecnicos.Api/Program.cs
@@ -10,13 +10,13 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
-
+app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
